025b5aecc23bf2f8780cd0fc97933e4aa7644c1e
flavienrak <flavien.andrisoarak@gmail.com>
2025-06-04 17:03:26 +0300

    feat(init): add initial configs

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+  return (
+    <div className="w-full h-full min-h-screen text-[var(--text-primary-color)] [background-image:var(--bg-primary)]">
+      Landing Page
+    </div>
+  );
+}
